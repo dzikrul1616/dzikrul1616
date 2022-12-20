838bf7d079d07a805e369060b515dd0053c52778
M. Dzikrull Hakim <91420806+dzikrul1616@users.noreply.github.com>
2022-12-21 01:07:55 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,4 +1,10 @@
 //haloo wkwk
 console.log("Hello World")
 console.log("Hello World")
-console.log("Hello World")console.log("Hello World")console.log("Hello World")
+console.log("Hello World")
+
+function main () {
+ return null;
+}
+main()
+console.log(main)
