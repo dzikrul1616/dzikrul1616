b389c20dabf044a29e979bd9095218130ca9d767
M. Dzikrull Hakim <91420806+dzikrul1616@users.noreply.github.com>
2022-12-21 01:09:30 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,4 +1,10 @@
 //haloo wkwk
 console.log("Hello World")
 console.log("Hello World")
-console.log("Hello World")console.log("Hello World")console.log("Hello World")
+console.log("Hello World")
+
+function main() {
+  retun null;
+}
+
+
